757d8aedf099cd7e0cc983bb5a62491a059660c0
Oscar Lozano <olozano1194@gmail.com>
2024-10-04 14:42:25 -0500

    ....

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './src/pages/**/*.html',
+      './src/api/**/*.js',
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
+
